9713c3636414723fcc0ac418cfe87caca4f2100d
Larch <40319009+larch13@users.noreply.github.com>
2024-02-06 23:59:32 -0600

    Open server with correct launch options

--- Window.cs
+++ Window.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace WildOmissionServerConfigurator
 {
     public partial class Window : Form
@@ -15,8 +17,10 @@ namespace WildOmissionServerConfigurator
         private void StartServerButton_Click(object sender, EventArgs e)
         {
             // TODO create server administrators ini file
-            // TODO launch server executable with correct launch options
 
+            // Launch server executable with correct launch options
+            String LaunchString = String.Format("WildOmissionServer.exe -log -SteamServerName={0} -Port={1}", ServerNameTextBox.Text, PortTextBox.Text);
+            Process.Start(LaunchString);
         }
 
         private void CancelButton_Click(object sender, EventArgs e)
